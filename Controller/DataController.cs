f42c46d22a818698e55acfb06cb3670b2ec1dd00
Faraz Ahmad <imfaraz101@gmail.com>
2024-03-25 12:27:43 +0500

    Get All Customer Orders

--- Controller/DataController.cs
+++ Controller/DataController.cs
@@ -14,6 +14,39 @@ public class DataController : ControllerBase
         WebHostEnvironment = webHostEnvironment;
     }
 
+    [HttpGet("customerOrders")]
+    public ActionResult<IEnumerable<CustomerOrderDto>> GetAllCustomerOrders()
+    {
+        var customerPath = WebHostEnvironment.ContentRootPath + "\\MyAppData\\csv\\Customers.csv";
+        var orderPath = WebHostEnvironment.ContentRootPath + "\\MyAppData\\csv\\Orders.csv";
+        var orderItemPath = WebHostEnvironment.ContentRootPath + "\\MyAppData\\csv\\OrderItems.csv";
+        var customers = _dataService.ReadCSV<Customer>(customerPath);
+        var orders = _dataService.ReadCSV<Order>(orderPath);
+        var orderItems = _dataService.ReadCSV<OrderItem>(orderItemPath);
+
+
+        var customerOrders = customers.Select(customer => new CustomerOrderDto
+        {
+            CustomerId = customer.customer_id,
+            FirstName = customer.first_name,
+            LastName = customer.last_name,
+            Orders = orders.Where(order => order.customer_id == customer.customer_id)
+                                    .Select(order => new OrderDto
+                                    {
+                                        OrderId = order.order_id,
+                                        CustomerId = order.customer_id,
+                                        OrderDate = order.order_date,
+                                        ShippedDate = order.shipped_date,
+                                        RequiredDate = order.required_date,
+                                        OrderItems = orderItems.Where(item => item.order_id == order.order_id)
+                                                                .ToList()
+                                    }).ToList()
+        }).ToList();
+
+        return customerOrders;
+    }
+
+
     [HttpGet("customers")]
     public ActionResult<IEnumerable<Customer>> GetCustomers()
     {
@@ -33,8 +66,27 @@ public class DataController : ControllerBase
     [HttpGet("orderItems")]
     public ActionResult<IEnumerable<OrderItem>> GetOrderItems()
     {
-        var serverPath = WebHostEnvironment.ContentRootPath + "\\MyAppData\\csv\\OrderItem.csv";
+        var serverPath = WebHostEnvironment.ContentRootPath + "\\MyAppData\\csv\\OrderItems.csv";
         var orderItems = _dataService.ReadCSV<OrderItem>(serverPath);
         return Ok(orderItems);
+    }
+
+    public class CustomerOrderDto
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<OrderDto> Orders { get; set; }
+    }
+
+    public class OrderDto
+    {
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
+
+        public string OrderDate { get; set; }
+        public string RequiredDate { get; set; }
+        public string ShippedDate { get; set; }
     }
 }
--- obj/Debug/net8.0/CSVBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/CSVBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSVBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce4ca39c370dd571d37e939e61fdc4d646f622bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSVBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSVBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
